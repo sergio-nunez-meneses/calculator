a4ce39159e699856620a835464049939fbefbcf2
sergionunezmeneses <snunezmeneses@gmail.com>
2022-03-07 17:40:03 +0100

    Add namespace, and functions consoleLog and stringifyVector in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+void consoleLog(const string &str)
+{
+    cout << str << endl;
+}
+
+string stringifyVector(vector<char> &v)
+{
+	string stringify;
+
+	if (!v.empty())
+	{
+		short int i = 0;
+
+		stringify = "[";
+		for (const auto &element : v)
+		{
+			stringify += "\"";
+			stringify += element;
+			stringify += "\"";
+
+			if (i < v.size() - 1)
+			{
+				stringify += ", ";
+			}
+			++i;
+		}
+		stringify += "]";
+	}
+	return stringify;
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
