33ba425153ef069dccebb2f6339cc768e99fd6e6
sergionunezmeneses <snunezmeneses@gmail.com>
2022-03-08 14:29:58 +0100

    Add conversion ASCII char to int in function calculator in main.cpp

--- main.cpp
+++ main.cpp
@@ -51,7 +51,15 @@ void calculator(const string &str)
 		if (isspace(str[i]))
 			continue;
 		else if (isdigit(str[i]))
-			operands.push_back(str[i]);
+		{
+			// Subtract ASCII character by '0' to get numeric value, and multiply it by 10 to shuffle digits to the left
+			int result = 0;
+
+			for (; isdigit(str[i]); ++i)
+				result = (result * 10) + (str[i] - '0');
+
+			operands.push_back(result);
+		}
 		else
 			operators.push_back(str[i]);
 	}
