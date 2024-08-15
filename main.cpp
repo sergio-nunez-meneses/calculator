d17c377c696cf7c4ea7984bc1edeb090ffe7dd90
sergionunezmeneses <snunezmeneses@gmail.com>
2024-08-15 11:44:44 +0200

    Add class declaration (main.cpp)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,23 @@
 
 using namespace std;
 
+class ReversePolishNotation
+{
+public:
+	explicit ReversePolishNotation(string &input);
+
+	string display();
+
+private:
+	deque<string> ordered_tokens;
+
+	static bool isValidOper(char t_oper);
+
+	static map<char, string> operInfo(char t_oper);
+
+	void convert(const string &tokens);
+};
+
 bool isValidOper(char t_oper)
 {
 	vector<char> validOpers = {'^', '*', '/', '+', '-'};
